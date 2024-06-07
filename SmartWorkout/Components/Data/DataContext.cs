ed9e5b69b7f36d125ffb2f10978a7bdbde407fa3
Ethan Page <ethanpage101@gmail.com>
2024-06-07 13:31:23 -0500

    Add project files.

---
+++ SmartWorkout/Components/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartWorkout.Components.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+}
